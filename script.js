8b9a48a960eb0904c732e0f85da17ebca5c55add
Abdel <abdelbarka96@gmail.com>
2024-08-21 15:52:43 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,10 +1,5 @@
 const tips = document.querySelectorAll(".tip");
-const customBtn = document.querySelector('.customBtn');
-const customInput = document.querySelector('.customInput');
 
-const hideCustomInput = (input)=>{
-    input.classList.add('hidden');
-}
 
 const removeSelectionStyle = (items)=>{
     items.forEach((item)=>{
@@ -16,16 +11,9 @@ const handleStyleOnClick = (item, func, items)=>{
     item.addEventListener('click', (e)=>{
         func(items);
         e.target.classList.add('btnSelected');
-        if(e.target.textContent.toLowerCase()!='custom'){
-            hideCustomInput(customInput);
-        }
     })
 }
 
 tips.forEach((tip)=>{
     handleStyleOnClick(tip, removeSelectionStyle, tips);
 });
-
-customBtn.addEventListener('click',()=>{
-    customInput.classList.toggle('hidden');
-})
