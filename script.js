2eef3b1f1f62aa491584a2050701aaf3a6004ecf
Abdel <abdelbarka96@gmail.com>
2024-08-21 12:08:28 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,17 +1,18 @@
 const tips = document.querySelectorAll(".tip");
 
-const removeSelectionStyle = ()=>{
-    tips.forEach((tip)=>{
-        tip.classList.remove('btnSelected')
+const removeSelectionStyle = (items)=>{
+    items.forEach((item)=>{
+        item.classList.remove('btnSelected')
     })
 }
 
-const handleStyleOnClick = (item, func)=>{
+const handleStyleOnClick = (item, func, items)=>{
     item.addEventListener('click', (e)=>{
-        func();
+        func(items);
         e.target.classList.add('btnSelected');
     })
 }
+
 tips.forEach((tip)=>{
-    handleStyleOnClick(tip, removeSelectionStyle);
+    handleStyleOnClick(tip, removeSelectionStyle, tips);
 });
